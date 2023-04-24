7c89036ef3cb47ea21d97c89d76c4d6090c2292f
skhomuti <skhomuti@gmail.com>
2023-04-24 12:10:20 +0500

    test

--- wallets-testing/playwright.config.ts
+++ wallets-testing/playwright.config.ts
@@ -41,16 +41,16 @@ const config: PlaywrightTestConfig = {
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 120000,
-    screenshot: 'only-on-failure',
+    screenshot: 'on',
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    // trace: 'on-first-retry',
+    trace: 'on',
   },
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  outputDir: 'test-results/',
+  // outputDir: 'test-results/',
 };
 
 export default config;
