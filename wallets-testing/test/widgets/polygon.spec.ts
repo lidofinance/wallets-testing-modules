e617f55c6ca701ba3fda4b11ac77469b288bf386
itaven <vladk520@gmail.com>
2023-10-19 16:17:48 +0300

    feat: add taho connection test to polygon

--- wallets-testing/test/widgets/polygon.spec.ts
+++ wallets-testing/test/widgets/polygon.spec.ts
@@ -9,8 +9,12 @@ import {
   EXODUS_COMMON_CONFIG,
   OKX_COMMON_CONFIG,
   BITGET_COMMON_CONFIG,
+  TAHO_COMMON_CONFIG,
 } from '@lidofinance/wallets-testing-wallets';
-import { POLYGON_WIDGET_CONFIG } from '@lidofinance/wallets-testing-widgets';
+import {
+  ETHEREUM_WIDGET_CONFIG,
+  POLYGON_WIDGET_CONFIG,
+} from '@lidofinance/wallets-testing-widgets';
 import { BrowserModule } from '../../browser/browser.module';
 import { BrowserService } from '../../browser/browser.service';
 import { MATIC_TOKEN } from './consts';
@@ -69,6 +73,11 @@ test.describe('Polygon', () => {
 
   test(`BitGet connect`, async () => {
     await browserService.setup(BITGET_COMMON_CONFIG, POLYGON_WIDGET_CONFIG);
+    await browserService.connectWallet();
+  });
+
+  test(`Taho connect`, async () => {
+    await browserService.setup(TAHO_COMMON_CONFIG, POLYGON_WIDGET_CONFIG);
     await browserService.connectWallet();
   });
 
