c4b8903819996e6fe954dd7025929c97e6ab3c4a
itaven <vladk520@gmail.com>
2023-10-26 12:09:42 +0500

    debug: check url

--- packages/wallets/src/metamask/metamask.page.ts
+++ packages/wallets/src/metamask/metamask.page.ts
@@ -2,6 +2,7 @@ import { WalletConfig } from '../wallets.constants';
 import { WalletPage } from '../wallet.page';
 import expect from 'expect';
 import { test, BrowserContext, Page } from '@playwright/test';
+import * as process from 'process';
 
 export class MetamaskPage implements WalletPage {
   page: Page | undefined;
@@ -29,9 +30,9 @@ export class MetamaskPage implements WalletPage {
     await test.step('Setup', async () => {
       await this.navigate();
       if (!this.page) throw "Page isn't ready";
+      process.stdout.write(this.page.url());
       await this.page.waitForTimeout(20000);
-      await this.page.screenshot({ fullPage: true });
-      console.log(await this.page.content());
+      process.stdout.write(await this.page.content());
       const firstTime =
         (await this.page.locator('data-testid=onboarding-welcome').count()) > 0;
       if (firstTime) await this.firstTimeSetup();
