4052103648f3e0935afb10ea1ba4c238f3d40cfa
itaven <vlad.k@lido.fi>
2024-05-20 14:42:17 +0400

    fix: rollback debug options

--- wallets-testing/playwright.config.ts
+++ wallets-testing/playwright.config.ts
@@ -23,9 +23,9 @@ const config: PlaywrightTestConfig = {
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: false,
+  forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 0 : 0,
+  retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
