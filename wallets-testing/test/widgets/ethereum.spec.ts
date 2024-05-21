52b4467dd669a2a20af802343894654c3986987d
itaven <vlad.k@lido.fi>
2024-05-21 18:18:56 +0400

    fix: remove debugging

--- wallets-testing/test/widgets/ethereum.spec.ts
+++ wallets-testing/test/widgets/ethereum.spec.ts
@@ -29,7 +29,7 @@ test.describe('Ethereum', () => {
     browserService = moduleFixture.get<BrowserService>(BrowserService);
   });
 
-  test.only(`Metamask stake`, async () => {
+  test(`Metamask stake`, async () => {
     await browserService.setupWithNode(
       METAMASK_COMMON_CONFIG,
       ETHEREUM_WIDGET_CONFIG,
