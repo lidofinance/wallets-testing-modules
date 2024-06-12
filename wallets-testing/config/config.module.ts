15d8e4770ce1765febbfb87246302f950821ed19
Aleksandr <captain_sparrow@lido.fi>
2024-06-13 00:49:30 +0800

    Fix type

--- wallets-testing/config/config.module.ts
+++ wallets-testing/config/config.module.ts
@@ -1,8 +1,9 @@
 import { ConfigModule as ConfigModuleSource } from '@nestjs/config';
 import { ConfigService } from './config.service';
 import { validate } from './env.validation';
+import { DynamicModule } from '@nestjs/common';
 
-export const ConfigModule = ConfigModuleSource.forRoot({
+export const ConfigModule: DynamicModule = ConfigModuleSource.forRoot({
   validate,
   isGlobal: true,
   cache: true,
