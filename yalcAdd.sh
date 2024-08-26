e01871f958a4b3404f362811663a59dcd1553264
Jake <93612860+jake4take@users.noreply.github.com>
2024-08-26 14:31:16 +0300

    chore: Delete yalcAdd.sh

--- yalcAdd.sh
+++
@@ -1,4 +0,0 @@
-yalc add @lidofinance/wallets-testing-widgets
-yalc add @lidofinance/wallets-testing-wallets
-yalc add @lidofinance/wallets-testing-wallets
-yalc add @lidofinance/wallets-testing-extensions
