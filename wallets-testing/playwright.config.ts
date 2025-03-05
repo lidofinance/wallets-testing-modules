049d3856ace09bacc84dd07104ff41fb09bafc15
jake <jake@lido.fi>
2025-03-05 15:47:32 +0300

    fix: some fixes

--- wallets-testing/playwright.config.ts
+++ wallets-testing/playwright.config.ts
@@ -1,5 +1,4 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
-import 'reflect-metadata';
 
 /**
  * Read environment variables from file.
@@ -49,6 +48,7 @@ const config: PlaywrightTestConfig = {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
+
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
   // outputDir: 'test-results/',
 };
