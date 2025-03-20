f93c3e53e11d725684f26014f41692d2bcc5133f
Aleksandr <captain_sparrow@lido.fi>
2025-03-20 23:52:34 +0300

    fix: update tests for new wallets versions

--- wallets-testing/playwright.config.ts
+++ wallets-testing/playwright.config.ts
@@ -27,7 +27,7 @@ const config: PlaywrightTestConfig = {
   /* Retry on CI only */
   retries: process.env.CI ? 0 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: 4,
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI
     ? [
