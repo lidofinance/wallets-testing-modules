e35de4f9a22ccf0b1235f2b649c72c0800655edc
jake <jake@lido.fi>
2025-05-19 23:28:54 +0300

    fix: remove test data

--- packages/wallets/src/EOA/metamask/pages/elements/popover.element.ts
+++ packages/wallets/src/EOA/metamask/pages/elements/popover.element.ts
@@ -1,8 +1,6 @@
 import { Locator, Page, test } from '@playwright/test';
-import { ConsoleLogger } from '@nestjs/common';
 
 export class PopoverElements {
-  logger = new ConsoleLogger('Metamask. ' + PopoverElements.name);
   popoverCloseButton: Locator;
   manageInSettingButton: Locator;
   notRightNowButton: Locator;
@@ -32,22 +30,21 @@ export class PopoverElements {
       if (!this.page) throw "Page isn't ready";
 
       if (await this.solanaPopupCloseBtn.isVisible())
-        await this.solanaPopupCloseBtn.click({ timeout: 3000 });
+        await this.solanaPopupCloseBtn.click();
 
-      if (await this.isPopoverVisible())
-        await this.popoverCloseButton.click({ timeout: 3000 });
+      if (await this.isPopoverVisible()) await this.popoverCloseButton.click();
 
       if (await this.manageInSettingButton.isVisible())
-        await this.manageInSettingButton.click({ timeout: 3000 });
+        await this.manageInSettingButton.click();
 
       if (await this.notRightNowButton.isVisible())
-        await this.notRightNowButton.click({ timeout: 3000 });
+        await this.notRightNowButton.click();
 
       if (await this.gotItButton.first().isVisible())
-        await this.gotItButton.first().click({ timeout: 3000 });
+        await this.gotItButton.first().click();
 
       if (await this.noThanksButton.isVisible())
-        await this.noThanksButton.click({ timeout: 3000 });
+        await this.noThanksButton.click();
     });
   }
 
