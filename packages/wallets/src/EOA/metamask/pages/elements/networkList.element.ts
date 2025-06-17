77c9b3835ebe95eff9d0f15fbcb4f334267027bd
Aleksandr <captain_sparrow@lido.fi>
2025-06-17 12:37:04 -0300

    fix: fix

--- packages/wallets/src/EOA/metamask/pages/elements/networkList.element.ts
+++ packages/wallets/src/EOA/metamask/pages/elements/networkList.element.ts
@@ -62,9 +62,8 @@ export class NetworkList {
     const testIdPrefix = 'network-list-item-options-button-';
     // or locator used for different MM versions from latest and LATEST_STABLE_DOWNLOAD_LINK
     const modalNetworkEditButton = this.dialogSection
-      .getByTestId(`${testIdPrefix}eip155:${hexChainId}`)
-      .or(this.dialogSection.getByTestId(`${testIdPrefix}0x${hexChainId}`))
-      .or(this.dialogSection.getByTestId(`${testIdPrefix}eip155:${chainId}`));
+      .getByTestId(`${testIdPrefix}eip155:${hexChainId}`) // old stable version
+      .or(this.dialogSection.getByTestId(`${testIdPrefix}eip155:${chainId}`)); // new latest version
     await modalNetworkEditButton.click();
     await this.editNetworkButton.click();
   }
