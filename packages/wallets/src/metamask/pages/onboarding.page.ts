8fd1198b4652572ced9edf87754de52953673d48
itaven <vlad.k@lido.fi>
2025-07-08 20:22:29 +0300

    fix: update onboarding flow

--- packages/wallets/src/metamask/pages/onboarding.page.ts
+++ packages/wallets/src/metamask/pages/onboarding.page.ts
@@ -1,70 +1,82 @@
 import { Locator, Page, test } from '@playwright/test';
 import { AccountConfig } from '../../wallets.constants';
 
-export class OnboardingPage {
+export class OnboardingPageV2 {
+  getStartedButton: Locator;
+  termsOfUseScrollButton: Locator;
   termsCheckboxButton: Locator;
+  termsOfUseAgreeButton: Locator;
   importWalletButton: Locator;
   metricAgreeButton: Locator;
-  secretPhraseInputs: Locator;
+  secretPhraseTextArea: Locator;
   secretPhraseImportButton: Locator;
   createPasswordInput: Locator;
   confirmPasswordInput: Locator;
   createPasswordTerms: Locator;
-  createPasswordImport: Locator;
+  createPasswordSubmit: Locator;
   completeButton: Locator;
-  pinExtensionNextButton: Locator;
   pinExtensionDoneButton: Locator;
 
   constructor(public page: Page, public accountConfig: AccountConfig) {
-    this.termsCheckboxButton = this.page.getByTestId(
-      'onboarding-terms-checkbox',
+    this.getStartedButton = this.page.getByTestId(
+      'onboarding-get-started-button',
     );
+    this.termsOfUseScrollButton = this.page.getByTestId(
+      'terms-of-use-scroll-button',
+    );
+    this.termsOfUseAgreeButton = this.page.getByTestId(
+      'terms-of-use-agree-button',
+    );
+    this.termsCheckboxButton = this.page.locator('#terms-of-use__checkbox');
+
     this.importWalletButton = this.page.getByTestId('onboarding-import-wallet');
     this.metricAgreeButton = this.page.getByTestId('metametrics-i-agree');
-    this.secretPhraseInputs = this.page.locator(
-      '.import-srp__srp-word >> input[type=password]',
+    this.secretPhraseTextArea = this.page.getByTestId(
+      'srp-input-import__srp-note',
     );
     this.secretPhraseImportButton = this.page.getByTestId('import-srp-confirm');
-    this.createPasswordInput = this.page.getByTestId('create-password-new');
+    this.createPasswordInput = this.page.getByTestId(
+      'create-password-new-input',
+    );
     this.confirmPasswordInput = this.page.getByTestId(
-      'create-password-confirm',
+      'create-password-confirm-input',
     );
     this.createPasswordTerms = this.page.getByTestId('create-password-terms');
-    this.createPasswordImport = this.page.getByTestId('create-password-import');
+    this.createPasswordSubmit = this.page.getByTestId('create-password-submit');
     this.completeButton = this.page.getByTestId('onboarding-complete-done');
-    this.pinExtensionNextButton = this.page.getByTestId('pin-extension-next');
     this.pinExtensionDoneButton = this.page.getByTestId('pin-extension-done');
   }
 
   async firstTimeSetup() {
-    await test.step('First time wallet setup', async () => {
+    await test.step('First time wallet setup (v2)', async () => {
+      await this.getStartedButton.click();
       await this.confirmTermsOfOnboarding();
       await this.importWalletButton.click();
-      await this.metricAgreeButton.click();
       await this.fillSecretPhrase(this.accountConfig.SECRET_PHRASE);
       await this.secretPhraseImportButton.click();
       await this.createPassword(this.accountConfig.PASSWORD);
+      await this.metricAgreeButton.click();
       await this.completeButton.click();
-      await this.pinExtensionNextButton.click();
       await this.pinExtensionDoneButton.click();
-      await this.page.waitForURL('**/home.html#');
+      await this.page.waitForURL('**/home.html#unlock');
     });
   }
 
   async confirmTermsOfOnboarding() {
     await test.step('Confirm terms before onboarding', async () => {
-      while (!(await this.page.locator('.check-box__checked').isVisible())) {
+      await this.termsOfUseScrollButton.click();
+      while (
+        !(await this.page.locator('.mm-checkbox__input--checked').isVisible())
+      ) {
         await this.termsCheckboxButton.click();
       }
+      await this.termsOfUseAgreeButton.click();
     });
   }
 
   async fillSecretPhrase(secretPhrase: string) {
     await test.step('Fill onboarding secret phrase field', async () => {
-      const seedWords = secretPhrase.split(' ');
-      for (let i = 0; i < seedWords.length; i++) {
-        await this.secretPhraseInputs.nth(i).fill(seedWords[i]);
-      }
+      await this.secretPhraseTextArea.pressSequentially(secretPhrase);
     });
   }
 
@@ -73,7 +85,7 @@ export class OnboardingPage {
       await this.createPasswordInput.fill(password);
       await this.confirmPasswordInput.fill(password);
       await this.createPasswordTerms.click();
-      await this.createPasswordImport.click();
+      await this.createPasswordSubmit.click();
     });
   }
 }
