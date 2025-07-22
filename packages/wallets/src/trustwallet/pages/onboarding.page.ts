737254c4365227b92f805f86a5babb837adaa6f0
itaven <vlad.k@lido.fi>
2025-07-22 10:42:41 +0300

    fix: add trust new onboarding flow

--- packages/wallets/src/trustwallet/pages/onboarding.page.ts
+++ packages/wallets/src/trustwallet/pages/onboarding.page.ts
@@ -4,73 +4,70 @@ import { ConsoleLogger } from '@nestjs/common';
 
 export class OnboardingPage {
   logger = new ConsoleLogger(`TrustWallet. ${OnboardingPage.name}`);
+  onboardingTitle: Locator;
+  secureWalletOptionPassword: Locator;
+  termsOfUseCheckbox: Locator;
   importWalletBtn: Locator;
   newPasswordInput: Locator;
-  agreementCheckbox: Locator;
-  nextBtn: Locator;
-  seedPhraseTypeInput: Locator;
-  seedPhraseInputs: Locator;
-  noThanksBtn: Locator;
+  confirmBtn: Locator;
+  seedPhraseTextArea: Locator;
 
   constructor(public page: Page, public accountConfig: AccountConfig) {
-    this.importWalletBtn = this.page.getByText('Recover with mnemonic');
+    this.onboardingTitle = this.page.getByTestId('onboarding-step-title');
+    this.secureWalletOptionPassword = this.page
+      .getByRole('button')
+      .getByText('Password');
+    this.termsOfUseCheckbox = this.page.getByRole('checkbox');
+    this.importWalletBtn = this.page.getByText('I already have a wallet');
     this.newPasswordInput = this.page.getByTestId('password-field');
-    this.agreementCheckbox = this.page.locator('input[type=checkbox]');
-    this.nextBtn = this.page.locator('button:has-text("Next")');
-    this.seedPhraseTypeInput = this.page.locator(
-      '[id="headlessui-listbox-button-:r2:"]',
-    );
-    this.seedPhraseInputs = this.page.locator('input[type="password"]');
-    this.noThanksBtn = this.page.locator('button:has-text("No thanks")');
+    this.confirmBtn = this.page.locator('button:has-text("Confirm")');
+    this.seedPhraseTextArea = this.page.locator('textarea');
   }
 
   async firstTimeSetup() {
-    await this.page.waitForSelector(
-      'text="Welcome to the Trust Wallet Extension"',
-    );
-
     await test.step('First time setup', async () => {
+      await this.onboardingTitle.waitFor({ state: 'visible' });
+      await this.termsOfUseCheckbox.click();
       await this.importWalletBtn.click();
-      try {
-        await this.newPasswordInput
-          .nth(0)
-          .waitFor({ timeout: 2000, state: 'visible' });
-        expect(await this.newPasswordInput.count()).toEqual(2);
-      } catch (er) {
-        this.logger.log('The wallet does not need to be configured');
-        return;
-      }
 
-      await test.step('Setup wallet password', async () => {
-        await this.newPasswordInput.nth(0).fill(this.accountConfig.PASSWORD);
-        await this.newPasswordInput.nth(1).fill(this.accountConfig.PASSWORD);
-        await this.agreementCheckbox.click();
-        await this.nextBtn.click();
-      });
+      await test.step('Step 1 of 3. Secure your wallet', async () => {
+        await this.page.getByRole('button').getByText('Password').click();
 
-      await test.step('Verify safety', async () => {
-        await this.agreementCheckbox.nth(0).check();
-        await this.agreementCheckbox.nth(1).check();
-        await this.agreementCheckbox.nth(2).check();
-        await this.nextBtn.click();
-      });
-
-      await test.step('Fill the seed phrase', async () => {
-        await this.seedPhraseTypeInput.waitFor({
-          state: 'visible',
-          timeout: 2000,
-        });
-        const seedWords = this.accountConfig.SECRET_PHRASE.split(' ');
-        for (let i = 0; i < seedWords.length; i++) {
-          await this.seedPhraseInputs.nth(i).fill(seedWords[i]);
+        try {
+          await this.newPasswordInput
+            .nth(0)
+            .waitFor({ timeout: 2000, state: 'visible' });
+          expect(await this.newPasswordInput.count()).toEqual(2);
+        } catch (er) {
+          this.logger.log('The wallet does not need to be configured');
+          return;
         }
-        await this.nextBtn.click();
+
+        await test.step('Setup wallet password', async () => {
+          await this.newPasswordInput.nth(0).fill(this.accountConfig.PASSWORD);
+          await this.newPasswordInput.nth(1).fill(this.accountConfig.PASSWORD);
+          await this.page.locator('button:has-text("Continue")').click();
+        });
       });
 
-      await this.noThanksBtn.click();
-      await this.page.waitForSelector(
-        'text=You have successfully imported your wallet!',
-      );
+      await test.step('Step 2 of 3. Import wallet', async () => {
+        await test.step('Select your existing wallet', async () => {
+          await this.page
+            .getByTestId('onboarding-select-import-method-trust-wallet')
+            .click();
+          await this.seedPhraseTextArea.waitFor({
+            state: 'visible',
+            timeout: 2000,
+          });
+          await this.seedPhraseTextArea.fill(this.accountConfig.SECRET_PHRASE);
+          await this.page.locator('button:has-text("Import")').click();
+          await this.onboardingTitle.waitFor({ state: 'visible' });
+        });
+      });
+
+      await test.step('Step 3 of 3.Confirm wallet created', async () => {
+        await this.page.locator('button:has-text("Open my wallet")').click();
+      });
     });
   }
 }
