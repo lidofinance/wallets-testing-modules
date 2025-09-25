472c8f12471f0e5bd07ec9d02a0729b194056119
Aleksandr <captain_sparrow@lido.fi>
2025-09-25 10:55:05 -0300

    feat: implement PgReporter class with metrics tracking and reporting functionality

---
+++ packages/pg-reporter/src/types.ts
@@ -0,0 +1,14 @@
+export interface ReportOptions {
+  appName: string;
+  env: string;
+  runName: string;
+  pushgatewayOptions: {
+    username: string;
+    password: string;
+    url: string;
+    cookie: string;
+  };
+  network: string;
+  testTags?: string;
+  skipProjects?: string[];
+}
