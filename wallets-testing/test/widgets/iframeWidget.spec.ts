ac69aadc414a3fd6363227e797144d7a35c70927
jake <jake@lido.fi>
2025-10-30 00:49:51 +0300

    chore: refactor

--- wallets-testing/test/widgets/iframeWidget.spec.ts
+++ wallets-testing/test/widgets/iframeWidget.spec.ts
@@ -12,95 +12,55 @@ import { BrowserService } from '@lidofinance/browser-service';
 import { getWidgetConfig } from '../../config';
 import { tokenToWithdraw, tokenToWrap } from '../../pages';
 
-test.describe(
-  'Test widget Lido app of Safe wallet (iframe) [Hoodi]',
-  { tag: '@hoodi' },
-  () => {
-    let browserService: BrowserService;
-    const txAmount = '0.0005';
+test.describe('Test widget Lido app of Safe wallet (iframe)', () => {
+  let browserService: BrowserService;
+  const config = String(process.env.SUITE).includes('mainnet')
+    ? {
+        isFork: false,
+        txAmount: '0.000001',
+        widgetConfig: getWidgetConfig['Ethereum'],
+        caseName: 'transaction initialization', // Check only tx initialization on the mainnet (without execution)
+      }
+    : {
+        isFork: false,
+        txAmount: '0.0005',
+        widgetConfig: getWidgetConfig['Ethereum Hoodi'],
+        caseName: 'transaction execution', // Check transaction execution
+      };
 
-    test.beforeAll(async () => {
-      const isFork = false;
-      browserService = await initBrowserWithExtension(
-        IFRAME_SAFE_COMMON_CONFIG,
-        isFork,
-        getWidgetConfig['Ethereum Hoodi'],
-      );
-    });
+  test.beforeAll(async () => {
+    browserService = await initBrowserWithExtension(
+      IFRAME_SAFE_COMMON_CONFIG,
+      config.isFork,
+      config.widgetConfig,
+    );
+  });
 
-    test('Stake', async () => {
-      await stake(browserService, txAmount);
-    });
+  test(`Stake ${config.caseName}`, async () => {
+    await stake(browserService, config.txAmount);
+  });
 
-    test('Wrap stETH', async () => {
-      await wrap(browserService, txAmount, tokenToWrap.stETH);
-    });
+  test(`Wrap stETH ${config.caseName}`, async () => {
+    await wrap(browserService, config.txAmount, tokenToWrap.stETH);
+  });
 
-    test('Wrap ETH', async () => {
-      await wrap(browserService, txAmount, tokenToWrap.ETH);
-    });
+  test(`Wrap ETH ${config.caseName}`, async () => {
+    await wrap(browserService, config.txAmount, tokenToWrap.ETH);
+  });
 
-    test('Unwrap wstETH', async () => {
-      await unwrap(browserService, txAmount);
-    });
+  test(`Unwrap wstETH ${config.caseName}`, async () => {
+    await unwrap(browserService, config.txAmount);
+  });
 
-    test('Request Withdraw stETH', async () => {
-      await request(browserService, txAmount, tokenToWithdraw.stETH);
-    });
+  test(`Request Withdraw stETH ${config.caseName}`, async () => {
+    await request(browserService, config.txAmount, tokenToWithdraw.stETH);
+  });
 
-    test('Request Withdraw wstETH', async () => {
-      await request(browserService, txAmount, tokenToWithdraw.wstETH);
-    });
+  test(`Request Withdraw wstETH ${config.caseName}`, async () => {
+    await request(browserService, config.txAmount, tokenToWithdraw.wstETH);
+  });
 
-    test('Claim', async () => {
-      await claim(browserService);
-    });
-  },
-);
-
-// Check only tx initialization on the mainnet (without execution)
-test.describe(
-  'Test widget Lido app of Safe wallet (iframe) [Ethereum Mainnet]',
-  { tag: '@mainnet' },
-  () => {
-    let browserService: BrowserService;
-    const txAmount = '0.000001';
-
-    test.beforeAll(async () => {
-      const isFork = false;
-      browserService = await initBrowserWithExtension(
-        IFRAME_SAFE_COMMON_CONFIG,
-        isFork,
-        getWidgetConfig['Ethereum'],
-      );
-    });
-
-    test('Init Stake tx', async () => {
-      await stake(browserService, txAmount);
-    });
-
-    test('Init Wrap stETH tx', async () => {
-      await wrap(browserService, txAmount, tokenToWrap.stETH);
-    });
-
-    test('Init Wrap ETH tx', async () => {
-      await wrap(browserService, txAmount, tokenToWrap.ETH);
-    });
-
-    test('Init Unwrap wstETH tx', async () => {
-      await unwrap(browserService, txAmount);
-    });
-
-    test('Init Request Withdraw stETH tx', async () => {
-      await request(browserService, txAmount, tokenToWithdraw.stETH);
-    });
-
-    test('Init Request Withdraw wstETH tx', async () => {
-      await request(browserService, txAmount, tokenToWithdraw.wstETH);
-    });
-
-    test('Init Claim tx', async () => {
-      await claim(browserService);
-    });
-  },
-);
+  test(`Claim ${config.caseName}`, async () => {
+    await claim(browserService);
+  });
+});
