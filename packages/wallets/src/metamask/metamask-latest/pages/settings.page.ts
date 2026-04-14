1223825f85ed88181772579ae020489911037312
jake <jake@lido.fi>
2026-04-14 20:35:34 +0300

    fix: fix locators in the setup process

--- packages/wallets/src/metamask/metamask-latest/pages/settings.page.ts
+++ packages/wallets/src/metamask/metamask-latest/pages/settings.page.ts
@@ -3,11 +3,7 @@ import { CommonWalletConfig } from '../../../wallets.constants';
 
 export class SettingsPage {
   tabBarMenu: Locator;
-  advancedTabButton: Locator;
-  experimentalTabButton: Locator;
-
-  inputNetworksForEachSiteToggle: Locator;
-  selectNetworksForEachSiteToggle: Locator;
+  preferencesTabButton: Locator;
 
   inputExtensionInFullSizeToggle: Locator;
   selectExtensionInFullSizeToggle: Locator;
@@ -17,25 +13,15 @@ export class SettingsPage {
     private extensionUrl: string,
     public walletConfig: CommonWalletConfig,
   ) {
-    this.tabBarMenu = this.page.locator('.tab-bar');
-    this.advancedTabButton = this.tabBarMenu
-      .getByRole('button')
-      .getByText('Advanced');
-    this.experimentalTabButton = this.tabBarMenu
-      .getByRole('button')
-      .getByText('Experimental');
-
-    // Experimental page locators
-    this.inputNetworksForEachSiteToggle = this.page
-      .getByTestId('experimental-setting-toggle-request-queue')
-      .locator('input');
-    this.selectNetworksForEachSiteToggle =
-      this.inputNetworksForEachSiteToggle.locator('..');
+    this.tabBarMenu = this.page.getByTestId('settings-v2-tab-bar-grouped');
+    this.preferencesTabButton = this.tabBarMenu.getByTestId(
+      'settings-v2-tab-item-preferences-and-display',
+    );
 
     // Advanced page locators
-    this.inputExtensionInFullSizeToggle = this.page
-      .getByTestId('advanced-setting-show-extension-in-full-size-view')
-      .locator('input');
+    this.inputExtensionInFullSizeToggle = this.page.getByTestId(
+      'show-extension-in-full-size-view',
+    );
     this.selectExtensionInFullSizeToggle =
       this.inputExtensionInFullSizeToggle.locator('..');
   }
@@ -53,7 +39,7 @@ export class SettingsPage {
   async enableFullSizeView() {
     await test.step('Enable the Extension in full size view toggle', async () => {
       await this.openSettings();
-      await this.advancedTabButton.click();
+      await this.preferencesTabButton.click();
 
       if (await this.inputExtensionInFullSizeToggle.isVisible()) {
         const toggleState =
