1cf7d88ee5003c7ae89eafc2af4f2571623fcb0a
jake <jake@lido.fi>
2026-04-16 18:16:13 +0300

    feat: skip Safe test

--- wallets-testing/test/widgets/connection.spec.ts
+++ wallets-testing/test/widgets/connection.spec.ts
@@ -1,11 +1,6 @@
 import {
   COIN98_COMMON_CONFIG,
-  TRUST_WALLET_COMMON_CONFIG,
-  COINBASE_COMMON_CONFIG,
   EXODUS_COMMON_CONFIG,
-  OKX_COMMON_CONFIG,
-  BITGET_COMMON_CONFIG,
-  CTRL_COMMON_CONFIG,
   WC_SAFE_COMMON_CONFIG,
   IFRAME_SAFE_COMMON_CONFIG,
   METAMASK_COMMON_CONFIG,
@@ -39,50 +34,50 @@ test.describe('Ethereum', () => {
   });
 
   // skip due to side panel
-  test.skip(`Trust connect`, async () => {
-    browserService = await initBrowserWithExtension({
-      walletConfig: TRUST_WALLET_COMMON_CONFIG,
-    });
-    await connectWallet(browserService);
-  });
+  // test.skip(`Trust connect`, async () => {
+  //   browserService = await initBrowserWithExtension({
+  //     walletConfig: TRUST_WALLET_COMMON_CONFIG,
+  //   });
+  //   await connectWallet(browserService);
+  // });
+  //
+  // test.skip(`Coinbase connect`, async () => {
+  //   browserService = await initBrowserWithExtension({
+  //     walletConfig: COINBASE_COMMON_CONFIG,
+  //   });
+  //   await connectWallet(browserService);
+  // });
+  //
+  // test.skip(`Ctrl connect`, async () => {
+  //   browserService = await initBrowserWithExtension({
+  //     walletConfig: CTRL_COMMON_CONFIG,
+  //   });
+  //   await connectWallet(browserService);
+  // });
+  //
+  // test.skip(`OKX connect`, async () => {
+  //   browserService = await initBrowserWithExtension({
+  //     walletConfig: OKX_COMMON_CONFIG,
+  //   });
+  //   await connectWallet(browserService);
+  // });
+  //
+  // // skip due to side panel
+  // test.skip(`Bitget connect`, async () => {
+  //   browserService = await initBrowserWithExtension({
+  //     walletConfig: BITGET_COMMON_CONFIG,
+  //   });
+  //   await connectWallet(browserService);
+  // });
 
-  test.skip(`Coinbase connect`, async () => {
-    browserService = await initBrowserWithExtension({
-      walletConfig: COINBASE_COMMON_CONFIG,
-    });
-    await connectWallet(browserService);
-  });
-
-  test.skip(`Ctrl connect`, async () => {
-    browserService = await initBrowserWithExtension({
-      walletConfig: CTRL_COMMON_CONFIG,
-    });
-    await connectWallet(browserService);
-  });
-
-  test.skip(`OKX connect`, async () => {
-    browserService = await initBrowserWithExtension({
-      walletConfig: OKX_COMMON_CONFIG,
-    });
-    await connectWallet(browserService);
-  });
-
-  // skip due to side panel
-  test.skip(`Bitget connect`, async () => {
-    browserService = await initBrowserWithExtension({
-      walletConfig: BITGET_COMMON_CONFIG,
-    });
-    await connectWallet(browserService);
-  });
-
-  test('WC_SDK+Safe connect', async () => {
+  test.skip('WC_SDK+Safe connect', async () => {
     browserService = await initBrowserWithExtension({
       walletConfig: WC_SAFE_COMMON_CONFIG,
     });
     await connectWallet(browserService);
   });
 
-  test('Safe IframeApp connect (with WC_SDK)', async () => {
+  test.skip('Safe IframeApp connect (with WC_SDK)', async () => {
     browserService = await initBrowserWithExtension({
       walletConfig: IFRAME_SAFE_COMMON_CONFIG,
     });
